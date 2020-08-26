6bafa419b978a2854f83fc3a304ea5cf03c2513d
alligatormonday <joey@joeyjepson.com>
2020-08-25 23:32:57 -0700

    Made password generator functional. Needs to validate user input.

--- script.js
+++ script.js
@@ -26,44 +26,80 @@ const special = "!@#$%^&*()"
 /**
  * generate a password based on certain criteria
  */
-function generatePassword(){
+function generatePassword() {
 
   // prompt for password criteria 
   // Collect data from user
-  let userLength = prompt ("Choose amount of characters between 8 to 128")
+  let userLength = prompt("Choose amount of characters between 8 to 128")
   // validate user responses 
-  let userLowercase = confirm("Do you want lowercase letters?")
+  let userLowercase = confirm("Do you want lowercase characters?")
+  let userUppercase = confirm("Do you want uppercase characters?")
+  let userNumeric = confirm("Do you want numeric characters?")
+  let userSpecial = confirm("Do you want special characters?")
   // console.log(userLength, userLowercase)
 
   // Criteria: 8 to 128 characters
 
   // Type of characters: lowercase, uppercase, numeric, and/or special characters
 
-  // Once prompts are completed: input validated based on user type selected
+  // Once confirmations are completed: input validated based on user type selected
   // Once all prompts are answered, then a password is generated that matches selected criteria
   // display as an alert
-    // What's our start data? A string? - How does a string pick one character within a string?
-    // We want the collection of data 
+  // What's our start data? A string? - How does a string pick one character within a string?
+  // We want the collection of data 
 
   // letters function?: create lowercase and uppercase properties within letters function. 
 
   // Password is currently blank! We need to make a better one
   let password = "";
-  passwordSelections = "";
+  let passwordSelections = "";
+
+  // Once all inputs have received. Generate an output for amount of characters user chose. 
+  // Based on users character choice, randomize the string of data for that given character/variable. 
+  // 
+  
+  userLength = parseInt(userLength)
+
   
 
-  if (userLowercase === true){
+  if (userLowercase === true) {
 
-  (passwordSelections + lowercase)
+    passwordSelections = passwordSelections + lowercase
 
   }
 
-   
+  if (userUppercase === true) {
 
+    passwordSelections = passwordSelections + uppercase
+
+  }
+
+  if (userNumeric === true) {
+
+    passwordSelections = passwordSelections + numeric
+
+  }
+
+  if (userSpecial === true) {
+
+    passwordSelections = passwordSelections + special
+
+  }
+
+  if (userLength >= 8 && userLength <= 128) {
+    
+    for (let i = 0; i < userLength; i++){
+      let randomCharacter = passwordSelections[Math.floor(Math.random() * passwordSelections.length)];
+      password = password + randomCharacter
+    }
+  }
+  
+  
+  
   // Code out for loop up until the desired character limit is reached, which is stored in the 'userLength' variable. 
   return password;
-  
-  
+
+
 
 }
 
